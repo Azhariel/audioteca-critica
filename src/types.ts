98f4462d41b0d2080f4341f1e27ab976b92db21b
Renan Braga <azhariel@gmail.com>
2023-04-09 22:16:02 -0300

    🏷️ types for rendered images

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,22 @@
+import { Layout } from 'gatsby-plugin-image'
+
 export interface Episode {
   id: string
   author: string[]
   title: string
   description: { description: string }
-  image: { url: string }
+  image: {
+    id: string
+    gatsbyImageData: {
+      images: {
+        sources: { srcSet: string; sizes: string; type: string }[]
+        placeholder: { fallback: string }
+      }
+      layout: Layout
+      width: number
+      height: number
+    }
+  }
   year: number
   episodeUrl: string
   textUrl: string
